47f1e84a1d8a0098f217a228a7277ecbef476678
Your Name <you@example.com>
2024-07-27 02:29:14 +0300

    project done

--- client/src/App/features/components/TelephoneList/TelephoneList.tsx
+++ client/src/App/features/components/TelephoneList/TelephoneList.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch } from "../../store/store";
 import TelephoneItem from "../TelephonesItem/TelephoneItem";
 import { getTelephones } from "../../actions/telephoneAction";
 import { loadTelephones } from "../../api/api";
+import styles from "./TelephoneList.module.css";
 
 function TelephoneList(): JSX.Element {
   const telephoneList = useSelector(
@@ -22,21 +23,31 @@ function TelephoneList(): JSX.Element {
   }, [dispatch]);
 
   return (
-    <div>
-      <ul>
-        {telephoneList.length ? (
-          telephoneList.map((telephone, index) => (
-            <TelephoneItem
-              key={telephone.id}
-              number={telephone.number}
-              index={index}
-              code={telephone.code}
-            />
-          ))
-        ) : (
-          <h1>No Telephones</h1>
-        )}
-      </ul>
+    <div className={styles.list}>
+      {telephoneList.length ? (
+        <>
+          <div className={styles.row}>
+            <span className={styles.row__index}>№</span>
+            <span className={styles.row__number}>Номер</span>
+            <span className={styles.row__country}>Страна</span>
+            <span className={styles.row__flag}>Флаг</span>
+          </div>
+          <ul className={styles.ul}>
+            {telephoneList.map((telephone, index) => (
+              <TelephoneItem
+                key={telephone.id}
+                number={telephone.number}
+                index={index + 1}
+                code={telephone.code}
+                countryName={telephone.countryName}
+                flag={telephone.flag}
+              />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h1>No Telephones</h1>
+      )}
     </div>
   );
 }
--- client/src/App/features/components/TelephonesItem/TelephoneItem.tsx
+++ client/src/App/features/components/TelephonesItem/TelephoneItem.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import styles from './TelephoneItem.module.css';
 
 type TelephoneItemProps = {
   number: string;
   index: number;
   code: string;
+  countryName: string;
+  flag: string;
 }
 
-function TelephoneItem ({ number, index, code }:TelephoneItemProps): JSX.Element {
+function TelephoneItem ({ number, index, code, countryName, flag }:TelephoneItemProps): JSX.Element {
   return (
-    <li className="item">
-      <strong>{index + 1}. </strong>
-      {`${code} (${number.slice(0, 3)}) ${number.slice(3)}`}
+    <li className={styles.item}>
+      <span className={styles.index}>{index} </span>
+      <span className={styles.number}>{`${code} (${number.slice(0, 3)}) ${number.slice(3)}`}</span>
+      <span className={styles.country}>{countryName}</span>
+      <span className={styles.flag}>{flag}</span>
     </li>
   );
 };
